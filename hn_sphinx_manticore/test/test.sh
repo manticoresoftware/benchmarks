d5d63da1534dab5c2d8b94520c5061275159cef1
djklim87 <djklim87@gmail.com>
2019-05-15 15:25:55 +0300

    tests 6 & 8 correction

--- hn_sphinx_manticore/test/test.sh
+++ hn_sphinx_manticore/test/test.sh
@@ -20,13 +20,13 @@ echo "TEST 5: - 2 terms each from group 600-750 under different concurrencies"
 echo "10000x 600-750 600-750" > queries_test.txt; php generate_queries.php queries_test.txt hn_top.txt|sort -R|uniq > q.txt; for engine in sphinx manticore; do for conc in 1 3 5 8 10 12 14; do php test.php --plugin=plain.php -b=100 --data=q.txt -c=$conc --host=hn_$engine --port=9306 --index=full --csv --tag=${engine}_${conc}; done; done;
 
 echo "TEST 6: - 3 terms from groups 100-200 400-500 800-900"
-echo "1000x 100-200 300-400 500-600 800-900" > queries_test.txt; php generate_queries.php queries_test.txt hn_top.txt|sort -R|uniq > q.txt; for engine in sphinx manticore; do php test.php --plugin=plain.php -b=100 --data=q.txt -c=1 --host=hn_$engine --port=9306 --index=full --csv --tag=${engine}; done;
+echo "1000x 100-200 400-500 800-900" > queries_test.txt; php generate_queries.php queries_test.txt hn_top.txt|sort -R|uniq > q.txt; for engine in sphinx manticore; do php test.php --plugin=plain.php -b=100 --data=q.txt -c=1 --host=hn_$engine --port=9306 --index=full --csv --tag=${engine}; done;
 
 echo "TEST 7: - 4 terms from groups 100-200 300-400 500-600 800-900"
 echo "1000x 100-200 300-400 500-600 800-900" > queries_test.txt; php generate_queries.php queries_test.txt hn_top.txt|sort -R|uniq > q.txt; for engine in sphinx manticore; do php test.php --plugin=plain.php -b=100 --data=q.txt -c=1 --host=hn_$engine --port=9306 --index=full --csv --tag=${engine}; done;
 
 echo "TEST 8: - 5 terms from groups 100-200 300-400 500-600 800-900 900-1000"
-echo "1000x 100-200 300-400 500-600 800-900" > queries_test.txt; php generate_queries.php queries_test.txt hn_top.txt|sort -R|uniq > q.txt; for engine in sphinx manticore; do php test.php --plugin=plain.php -b=100 --data=q.txt -c=1 --host=hn_$engine --port=9306 --index=full --csv --tag=${engine}; done;
+echo "1000x 100-200 400-500 800-900" > queries_test.txt; php generate_queries.php queries_test.txt hn_top.txt|sort -R|uniq > q.txt; for engine in sphinx manticore; do php test.php --plugin=plain.php -b=100 --data=q.txt -c=1 --host=hn_$engine --port=9306 --index=full --csv --tag=${engine}; done;
 
 echo "TEST 9: - 3 AND terms from groups 300-600 and 1 NOT from 300-400"
 echo "1000x 300-600 300-600 300-600 -300-400" > queries_test.txt; php generate_queries.php queries_test.txt hn_top.txt|sort -R|uniq > q.txt; for engine in sphinx manticore; do php test.php --plugin=plain.php -b=100 --data=q.txt -c=1 --host=hn_$engine --port=9306 --index=full --csv --tag=${engine}; done;
